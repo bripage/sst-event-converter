882b29e1e37099679bbfd7e2d966b0d88c12d977
Brian Page <bripage@users.noreply.github.com>
2023-02-21 10:50:34 -0500

    Update util.h

--- util.h
+++ util.h
@@ -1,5 +1,5 @@
-#ifndef MEMHIERARCHY_UTIL_H
-#define	MEMHIERARCHY_UTIL_H
+#ifndef MULTIBUS_UTIL_H
+#define	MULTIBUS_UTIL_H
 
 #include <sst/core/stringize.h>
 #include <sst/core/params.h>
@@ -34,90 +34,8 @@ namespace SST {
 #define _L11_ CALL_INFO,11,0    //Data values
 #define _L20_ CALL_INFO,20,0    //Debug at function call granularity
 
-// Type conversions - TODO are these used anywhere?
-        const unsigned int kibi = 1024;
-        const unsigned int mebi = kibi * 1024;
-        const unsigned int gibi = mebi * 1024;
-        const unsigned int tebi = gibi * 1024;
-        const unsigned int pebi = tebi * 1024;
-        const unsigned int exbi = pebi * 1024;
-
         typedef uint64_t Addr;
 
-// Event attributes
-/*
- *  Replace uB or UB (where u/U is a SI unit)
- *  with uiB or UiB for unit algebra
- */
-        inline void fixByteUnits(std::string &unitstr) {
-            trim(unitstr);
-            size_t pos;
-            string checkstring = "kKmMgGtTpP";
-            if ((pos = unitstr.find_first_of(checkstring)) != string::npos) {
-                pos++;
-                if (pos < unitstr.length() && unitstr[pos] == 'B') {
-                    unitstr.insert(pos, "i");
-                }
-            }
-        }
-
-        inline int log2Of(int x){
-            int temp = x;
-            int result = 0;
-            while(temp >>= 1) result++;
-            return result;
-        }
-
-        inline bool isPowerOfTwo(unsigned int x) {
-            return !(x & (x-1));
-        }
-
-/*
- * copy oldKey to newKey but don't overwrite if newKey already exists
- * return whether parameter was fixed
- */
-        inline bool fixupParam( SST::Params& params, const std::string oldKey, const std::string newKey ) {
-            bool found;
-            if (params.contains(newKey)) return false;
-
-            std::string value = params.find<std::string>(oldKey, found);
-            if (found) {
-                params.insert(newKey, value);
-                return true;
-                //    params.erase( oldKey );
-            }
-            return false;
-        }
-
-        inline void fixupParams( Params& params, const std::string oldKey, const std::string newKey ) {
-            // Params tmp = params.find_prefix_params( oldKey );
-
-            // std::set<std::string> keys = tmp.getKeys();
-            // std::set<std::string>::iterator iter = keys.begin();
-
-            std::set<std::string> keys = params.getKeys();
-            std::set<std::string>::iterator iter = keys.begin();
-            for ( ; iter != keys.end(); ++iter ) {
-                std::string prefix_key = iter->substr(0,oldKey.length());
-                if ( prefix_key == oldKey ) {
-                    std::string value = params.find<std::string>( (*iter) );
-                    params.insert( newKey + (iter->substr(oldKey.length())), value );
-                    //    params.erase( oldKey + (*iter) );
-                }
-            }
-        }
-/*
- *  IGNORE - ignore this request, drop it, do not retry any waiting requests
- *  DONE - this request finished, should retry
- *  STALL - this request is being handled and should be stalled in the MSHRs
- *  BLOCK - this request is blocked by a current outstanding request and should stall in the MSHRs
- *  REJECT - this request cannot be handled
- */
-        typedef enum {IGNORE, DONE, STALL, BLOCK, REJECT} CacheAction;
-
-        enum class CoherenceProtocol {MSI, MESI, NONE};
-
-        enum class Endpoint { CPU, Cache, Memory, Directory, Scratchpad, MMIO };
-
-    }}
+    }
+}
 #endif	/* UTIL_H */
