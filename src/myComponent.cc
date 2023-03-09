14be84f08bc175b11500dc019605c01dde6168fd
bripage <bpage1@nd.edu>
2023-03-09 14:41:52 -0500

    changes

--- src/myComponent.cc
+++ src/myComponent.cc
@@ -78,7 +78,7 @@ void MyComponent::handleMemoryEvent(SST::Event *ev) {
 
     } else {
         // Event is not a memEventBase event
-        SST::Interfaces::StandardMem::Request* memRequest = dynamic_cast<SST::Interfaces::StandardMem::Request*>(ev);
+        SST::Interfaces::SimpleMem::Request* memRequest = dynamic_cast<SST::Interfaces::SimpleMem::Request*>(ev);
         if (memRequest){
             // Event is actually a StandardMemory::Request
 
