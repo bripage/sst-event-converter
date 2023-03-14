37f6a2fe6cd2f3ce2db40b92ebf4682214b1a8c3
bripage <bpage1@nd.edu>
2023-03-14 09:25:34 -0400

    changes

--- src/myComponent.cc
+++ src/myComponent.cc
@@ -28,16 +28,20 @@ void MyComponent::configureLinks() {
 }
 
 void MyComponent::init(unsigned int phase) {
+    if (!rtr_0_Link)
+        out.fatal(CALL_INFO, -1, "%s, Error: unable to configure link on rtr_0_Link\n", getName().c_str());
 
+    if (!memoryLink)
+        out.fatal(CALL_INFO, -1, "%s, Error: unable to configure link memoryLink'\n", getName().c_str());
 }
 
 void MyComponent::handleNetworkEvent(SST::Event* ev) {
     // Extract the encapsulated MemEvent
     //out.fatal(CALL_INFO, -1, "%s, received something on port rtr_0\n", getName().c_str());
-    //memoryLink->send(ev);
+    memoryLink->send(ev);
 }
 
 void MyComponent::handleMemoryEvent(SST::Event *ev) {
     //out.fatal(CALL_INFO, -1, "%s, received somethign on port memory\n", getName().c_str());
-    //rtr_0_Link->send(ev);
+    rtr_0_Link->send(ev);
 }
